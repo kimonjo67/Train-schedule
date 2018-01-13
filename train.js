9cefcf07a43dfbde349e987fb64727261352dce3
kimonjo67 <monjokim@gmail.com>
2018-01-13 00:46:06 -0600

    first push

---
+++ train.js
@@ -0,0 +1,64 @@
+// Initializa firebase
+var config = {
+    apiKey: "AIzaSyD3hAqD8mkQs1myD_rjD8SP_kX6Hihn2Sg",
+    authDomain: "train-schedule-7c3d0.firebaseapp.com",
+    databaseURL: "https://train-schedule-7c3d0.firebaseio.com",
+    projectId: "train-schedule-7c3d0",
+    storageBucket: "train-schedule-7c3d0.appspot.com",
+    messagingSenderId: "739863430682"
+  };
+  firebase.initializeApp(config);
+
+  var database = firebase.database();
+  console.log("database created");
+
+  // Button for adding Trains
+  $("#add-train-btn").on("click", function(event) {
+  	event.preventDefault();
+  	//Grab User input
+  	var trainName = $("#train-name-input").val().trim();
+  	var trainDestination = $("#destination-input").val().trim();
+  	var trainFirst = $("#first-input").val().trim();
+
+  // Create a local temporary storage object for holding user input
+  var newTrain = {
+  	name: trainName,
+  	destination: trainDestination,
+  	first: trainFirst
+  };
+
+  //upload new train data to the database
+  database.ref().push(newTrain);
+
+  	console.log(newTrain.name);
+  	console.log(newTrain.destination);
+  	console.log(newTrain.first);
+
+  //Alert
+  alert("New train added here")
+
+  //Clear all of the text boxes
+  $("#train-name-input").val("");
+  $("#destination-input").val("");
+  $("#first-input").val("");
+ });
+
+  // Create a firebase event for adding Train to database and a row in the html
+  database.ref().on("child_added", function(childSnapshot, prevChildKey) {
+
+  console.log(childSnapshot.val());
+
+  // Store everything into a variable.
+  var trainName = childSnapshot.val().name;
+  var trainDestination = childSnapshot.val().destination;
+  var trainFirst = childSnapshot.val().first;
+
+  // Employee Info
+  console.log(trainName);
+  console.log(trainDestination);
+  console.log(trainFirst);
+
+  // Add each train's data into the table
+  $("#train-table > tbody").append("<tr><td>" + trainName + "</td><td>" + trainDestination + "</td><td>" +
+  trainFirst + "</td></tr>");
+});
